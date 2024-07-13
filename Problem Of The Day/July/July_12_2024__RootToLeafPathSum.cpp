4bab40d7eb534a37f883c79eabef27372ad77b99
Anurag Panda <anuragpanda2704@gmail.com>
2024-07-13 15:25:36 +0530

    3 questions solved

---
+++ Problem Of The Day/July/July_12_2024__RootToLeafPathSum.cpp
@@ -0,0 +1,26 @@
+// Level: EASY
+
+/* A binary tree node
+struct Node
+{
+    int data;
+    Node* left, * right;
+}; */
+
+/*you are required to
+complete this function */
+class Solution {
+    public:
+        bool hasPathSum(Node *root, int target) {
+            if(!root)
+                return 0;
+            
+            if(root->left==nullptr && root->right==nullptr){
+                if(root->data == target)
+                    return 1;
+                else return 0;
+            }
+            
+            return (hasPathSum(root->left, target-root->data)) || (hasPathSum(root->right, target-root->data));
+        }
+};
