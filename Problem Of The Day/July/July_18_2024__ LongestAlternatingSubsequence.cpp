1374d081b8cac8e71e452d4ed2ddc227137cd89b
Anurag Panda <anuragpanda2704@gmail.com>
2024-07-18 22:06:07 +0530

    1 POTD solved

---
+++ Problem Of The Day/July/July_18_2024__ LongestAlternatingSubsequence.cpp
@@ -0,0 +1,16 @@
+// Level: MEDIUM
+
+class Solution {
+    public:
+        int alternatingMaxLength(vector<int>& arr) {
+            int inc=1, dec=1;
+            for(int i=1; i<arr.size(); i++){
+                if(arr[i] > arr[i-1])
+                    inc = dec + 1;
+                else if(arr[i] < arr[i-1])
+                    dec = inc + 1;
+            }
+            
+            return max(inc, dec);
+        }
+};
