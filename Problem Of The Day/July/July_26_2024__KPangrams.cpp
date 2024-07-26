25cafb61f00d1837e3b964c442f11358e6e191f7
Anurag Panda <anuragpanda2704@gmail.com>
2024-07-26 21:47:42 +0530

    1 POTD solved

---
+++ Problem Of The Day/July/July_26_2024__KPangrams.cpp
@@ -0,0 +1,25 @@
+// Level: EASY
+
+class Solution {
+    public:
+        bool kPangram(string str, int k) {
+            set<char>st;
+            int count = 0;
+            for(int i=0;i<str.length();i++){
+                if(str[i]!=' ' and str[i]!=',' and str[i]!='.'){
+                    count++;
+                    st.insert(str[i]);
+                }
+            }
+            if(count < 26 ) 
+                return false;
+            
+            if(st.size()==26)
+                return true;
+            
+            if(26 - st.size() <= k) 
+                return true;
+            
+            return false;
+        }
+};
