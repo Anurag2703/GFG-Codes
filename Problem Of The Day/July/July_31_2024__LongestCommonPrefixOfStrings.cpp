a68ebb357a5a91debc6ca77bda2da6ce608f86ee
Anurag Panda <anuragpanda2704@gmail.com>
2024-07-31 21:25:36 +0530

    1 POTD solved

---
+++ Problem Of The Day/July/July_31_2024__LongestCommonPrefixOfStrings.cpp
@@ -0,0 +1,22 @@
+// Level: EASY
+
+class Solution {
+    public:
+        string longestCommonPrefix(vector<string> arr) {
+            int N = arr.size();
+            int i,j;
+            sort(arr.begin(), arr.end());
+            
+            string s1=arr[0], s2=arr[N-1], ans="";
+            for(int i=0; i<min(s1.size(), s2.size()); i++){
+                if(s1[i] == s2[i])
+                    ans += s1[i];
+                else break;
+            }
+            
+            if(ans == "")
+                return "-1";
+                
+            return ans;
+        }
+};
