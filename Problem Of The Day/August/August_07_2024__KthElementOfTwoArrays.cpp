412123e1a59cff141afa9e232f2f96625ff12ca0
Anurag Panda <anuragpanda2704@gmail.com>
2024-08-07 21:16:13 +0530

    1 POTD solved

---
+++ Problem Of The Day/August/August_07_2024__KthElementOfTwoArrays.cpp
@@ -0,0 +1,14 @@
+// Level: MEDIUM
+
+class Solution {
+    public:
+        int kthElement(int k, vector<int>& arr1, vector<int>& arr2) {
+            // code here
+            vector<int>ans(arr1.begin(),arr1.end());
+            
+            ans.insert(ans.end(),arr2.begin(),arr2.end());
+            sort(ans.begin(),ans.end());
+            
+            return ans[k-1];
+        }
+};
