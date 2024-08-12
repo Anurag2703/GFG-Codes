38856af71cec15e5d9e0ad05ed4de937fc950ca9
Anurag Panda <anuragpanda2704@gmail.com>
2024-08-12 20:56:45 +0530

    1 POTD solved

---
+++ Problem Of The Day/August/August_12_2024__SumOfMiddleElementsOfTwoSortedArrays.cpp
@@ -0,0 +1,26 @@
+// Level: MEDIUM
+
+//Position this line where user code will be pasted.
+class Solution {
+    public:
+        int SumofMiddleElements(vector<int> &arr1, vector<int> &arr2) {
+            // code here
+            int n = arr1.size();
+            int m = arr2.size();
+            vector<int> ans(n+m);
+            
+            for(int i=0; i<n; i++) {
+                ans[i] = arr1[i];
+            }
+            
+            for(int i=0; i<m; i++) {
+                ans[n+i] = arr2[i];
+            }
+            
+            sort(ans.begin(), ans.end());
+            
+            int rans = ans[(n+m)/2 - 1] + ans[(n+m)/2];
+            
+            return rans;
+        }
+};
