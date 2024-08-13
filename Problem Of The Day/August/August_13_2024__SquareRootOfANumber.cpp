fd80b3c7c678a6bd89303e7dd15a14f64e3866a5
Anurag Panda <anuragpanda2704@gmail.com>
2024-08-13 15:12:03 +0530

    1 POTD solved

---
+++ Problem Of The Day/August/August_13_2024__SquareRootOfANumber.cpp
@@ -0,0 +1,11 @@
+// Level: EASY
+
+// Function to find square root
+// x: element to find square root
+class Solution {
+    public:
+        long long int floorSqrt(long long int n) {
+            // Your code goes here
+            return floor(float(sqrt(n)));
+        }
+};
